785b2247c0738bb18758d6d4d43aa26d224a0d6a
james <danjame@hotmail.com>
2025-05-16 02:07:42 -0500

    Configure Clerk integration with dark theme appearance

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,17 @@ import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import react from "@astrojs/react";
 
+import { dark } from "@clerk/themes";
+
 export default defineConfig({
-	integrations: [clerk(), react()],
+	integrations: [
+		clerk({
+			appearance: {
+				baseTheme: dark,
+			},
+		}),
+		react(),
+	],
 	adapter: node({
 		mode: "standalone",
 	}),
