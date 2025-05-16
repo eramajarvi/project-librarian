d4e9517878d68ae2fa7e6ad3d30634770d6ef3ea
james <danjame@hotmail.com>
2025-05-16 08:14:02 -0500

    Refactor BookmarksFolders component and update styles for improved dropdown functionality and layout

--- src/components/BookmarksFolders.tsx
+++ src/components/BookmarksFolders.tsx
@@ -5,9 +5,32 @@ function BookmarksFolders() {
 		<div className="bookmarks-dropdown-wrapper">
 			{/* <label htmlFor="bookmarksFolders"></label> */}
 			<select id="bookmarksFolders" name="bookmarksFoldersSelector" className="bookmarks-dropdown">
-				<option value="one">Option One</option>
-				<option value="two">Option Two</option>
-				<option value="three">Option Three</option>
+				<button className="arrow-wrapper">
+					<selectedcontent></selectedcontent>
+					<div className="arrow-container">
+						<div className="arrow"></div>
+					</div>
+				</button>
+
+				<option value="one">
+					<p>🦁</p>
+					<div>general</div>
+				</option>
+
+				<option value="two">
+					<p>✨</p>
+					<div>Showgaze playlists playlistsplaylists</div>
+				</option>
+
+				<option value="three">
+					<p>😘</p>
+					<div>Tools</div>
+				</option>
+
+				<option value="four">
+					<p>🚨</p>
+					<div>components</div>
+				</option>
 			</select>
 		</div>
 	);
