7a5ae9392e2b8b09be44341635add50fe6a458ec
james <danjame@hotmail.com>
2025-05-19 11:36:51 -0500

    Refactor EmptyBookmarkPlaceholder component: add onAddBookmarkClick prop for better interactivity and improve structure

--- src/components/EmptyBookmarkPlaceholder.tsx
+++ src/components/EmptyBookmarkPlaceholder.tsx
@@ -4,40 +4,53 @@ import React from "react";
 import PlaceholderGradient from "../assets/SnapshotPlaceholderGradient.png";
 import PlaceholderCompass from "../assets/SnapshotPlaceholderCompass.png";
 
-const EmptyBookmarkPlaceholder: React.FC = () => (
-	<div
-		style={{
-			position: "relative",
-			width: "100%",
-			height: "100%",
-			display: "flex",
-			alignItems: "center",
-			justifyContent: "center",
-		}}>
-		<img
-			src={PlaceholderGradient.src}
-			alt="Background placeholder"
-			style={{
-				position: "absolute",
-				top: 0,
-				left: 0,
-				width: "100%",
-				height: "100%",
-				objectFit: "cover",
-				zIndex: 1,
-			}}
-		/>
-		<img
-			src={PlaceholderCompass.src}
-			alt="Foreground placeholder"
+interface EmptyBookmarkPlaceholderProps {
+	onAddBookmarkClick: () => void;
+}
+
+const EmptyBookmarkPlaceholder: React.FC<EmptyBookmarkPlaceholderProps> = ({ onAddBookmarkClick }) => {
+	return (
+		<div
 			style={{
 				position: "relative",
-				width: "120px",
-				height: "120px",
-				zIndex: 2,
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				cursor: "pointer",
 			}}
-		/>
-	</div>
-);
+			onClick={onAddBookmarkClick}
+			role="button"
+			tabIndex={0}
+			onKeyDown={(e) => {
+				if (e.key === "Enter" || e.key === " ") onAddBookmarkClick();
+			}}>
+			<img
+				src={PlaceholderGradient.src}
+				alt="Background placeholder"
+				style={{
+					position: "absolute",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%",
+					objectFit: "cover",
+					zIndex: 1,
+				}}
+			/>
+			<img
+				src={PlaceholderCompass.src}
+				alt="Foreground placeholder"
+				style={{
+					position: "relative",
+					width: "120px",
+					height: "120px",
+					zIndex: 2,
+				}}
+			/>
+		</div>
+	);
+};
 
 export default EmptyBookmarkPlaceholder;
