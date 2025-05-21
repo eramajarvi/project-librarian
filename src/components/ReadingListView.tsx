407a4f98b6dd4bb304d2bd830b15d046409791ef
james <danjame@hotmail.com>
2025-05-20 20:06:45 -0500

    feat: enhance ReadingListView with new layout and styles; add background images and improve text presentation

--- src/components/ReadingListView.tsx
+++ src/components/ReadingListView.tsx
@@ -1,3 +1,5 @@
+import "../styles/index.css";
+
 interface ReadingListViewProps {
 	username: string;
 	isOwner: boolean;
@@ -6,7 +8,15 @@ interface ReadingListViewProps {
 export default function ReadingListView({ username, isOwner }: ReadingListViewProps) {
 	return (
 		<div className="page-layout-base">
-			<div></div>
+			<div className="reading-list-container">
+				<div className="glasses-dark"></div>
+				<p className="reading-list-title pressed-heading">The Librarian</p>
+				<div className="pressed-heading reading-disclaimer">
+					<p>Una biblioteca retrofuturista para tus marcadores.</p>
+					<p>Este proyecto contiene componentes de código abierto.</p>
+					<p>CC BY-NC-SA 4.0 - eramajarvi 2025</p>
+				</div>
+			</div>
 		</div>
 	);
 }
